5e234982666828b0b01563d4a086aaae8d57199d
Vivian Morton <vivian.morton404@gmail.com>
2024-03-27 17:15:04 -0400

    Finished "working on the post detail page part 2"

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -1,8 +1,19 @@
-console.log('Hello world detail')
-
 const backBtn = document.getElementById('back-btn')
-
+const url = window.location.href
+const spinnerBox = document.getElementById('spinner-box')
 
 backBtn.addEventListener('click', ()=>{
     history.back()
+})
+
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response){
+        spinnerBox.classList.add('not-visible')
+    },
+    error: function(error){
+        console.log("Error",error)
+    }
 })
