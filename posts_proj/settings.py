d0d7afe2d68de472ecc2147f02283472abc82989
Vivian Morton <vivian.morton404@gmail.com>
2024-03-27 15:50:29 -0400

    Finished "adding a form to the modal" also installed crispy bootstrap 5 and updated settings.py

--- posts_proj/settings.py
+++ posts_proj/settings.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'posts',
     'profiles',
     #third party apps
-    'crispy_forms'
+    "crispy_forms",
+    "crispy_bootstrap5"
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
