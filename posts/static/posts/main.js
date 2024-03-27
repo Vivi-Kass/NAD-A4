b33b518f327c8c3cb38d4f5588b9657344add28f
Vivian Morton <vivian.morton404@gmail.com>
2024-03-27 16:56:43 -0400

    Finished "Creating post details page"

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -10,6 +10,8 @@ const csrf = document.getElementsByName('csrfmiddlewaretoken')
 console.log('csrf', csrf[0].value)
 const alertBox = document.getElementById('alert-box')
 
+const url = window.location.href
+
 
 const getCookie=(name) => {
     let cookieValue = null;
@@ -77,7 +79,7 @@ const getData = () => {
                             <div class="card-footer">
                                 <div class="row">
                                     <div class="col-1">
-                                        <a href="#" class="btn btn-primary">Details</a>
+                                        <a href="${url}${el.id}" class="btn btn-primary">Details</a>
                                     </div>
                                     <div class="col-2">
                                         <form class="like-unlike-forms" data-form-id="${el.id}">
