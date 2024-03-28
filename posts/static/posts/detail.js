021f2c5a4f4b4d901456a61641488744a647bce7
Vivian Morton <vivian.morton404@gmail.com>
2024-03-28 12:27:23 -0400

    Finished "working on the detail page part 4"

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -3,6 +3,7 @@ const url = window.location.href + 'data/'
 const spinnerBox = document.getElementById('spinner-box')
 const updateBtn = document.getElementById('update-btn')
 const deleteBtn = document.getElementById('delete-btn')
+const postBox = document.getElementById('post-box')
 
 backBtn.addEventListener('click', ()=>{
     history.back()
@@ -24,6 +25,18 @@ $.ajax({
             deleteBtn.classList.remove('not-visible')
             updateBtn.classList.remove('not-visible')
         }
+
+        const titleEl = document.createElement('h3')
+        titleEl.setAttribute('class', 'mt-3')
+
+        const bodyEl = document.createElement('p')
+        bodyEl.setAttribute('class', 'mt-1')
+
+        titleEl.textContent = data.title
+        bodyEl.textContent = data.body
+
+        postBox.appendChild(titleEl)
+        postBox.appendChild(bodyEl)
 
         spinnerBox.classList.add('not-visible')
     },
